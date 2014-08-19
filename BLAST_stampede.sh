d0389121e6d006b98af06063c884fd6973455cdd
Arun Seetharam <arnstrm@iastate.edu>
2014-08-19 16:53:07 -0500

    updated blast

--- BLAST_stampede.sh
+++ BLAST_stampede.sh
@@ -2,83 +2,24 @@
 # This script splits the large input fasta file and BLASTs them in parallel.
 # specifically, for the Stampede HPC cluster
 #
+# Change the BLAST parameters to adjust your needs
+blastparameters="blastp -max_target_seqs 1 -outfmt 6"
+#
+# NOTE: EXCLUDE '-db' AND '-query' OPTIONS
+#
 # Arun Seetharam <arnstrm@iastate.edu>, Genome Informatics Faciltiy
 # 19 August, 2014
 
-scriptName="${0##*/}"
-function printUsage() {
-    cat <<EOF
-
-Synopsis
-
-    $scriptName [-h | --help] -s splits path/to/fasta_file.fasta path/to/blast/database_name
-
-Description
-
-    This script splits the large input fasta file and BLASTs them in parallel, specifcally for the Stampede cluster
-    
-	-s, --splits=N
-        generate N number of splits to run the BLAST. Since each node has 16 processors in Stampede,
-        it is preferable to have splits as multiple of 16.		
-
-    -b, --blast-parameters=STRING
-        The BLAST type and parameters required for carrying out BLAST search. By default, "blastp -outfmt 6"	
-		will be used. Note that you don't have to specify '-db' AND '-query' options here
-		
-	path/to/fasta_file.fasta
-        full path to the FASTA file for using as BLAST query
-        script assumes the files have *.fasta extension
-
-    path/to/blast/database_name
-        full path to the BLAST database name (generated from ncbi makeblastdb)
-		Make sure that the path is absoulute
-
-    -h, --help
-        Brings up this help page
-
-Author
-
-    Arun Seetharam, Genome Informatics Facilty, Iowa State University
-    arnstrm@iastate.edu
-    19 August, 2014
-
-EOF
-}
-blastparameters="blastp -max_target_seqs 1 -outfmt 6"
-
-if [ $# -lt 1 ] ; then
-    printUsage
-    echo -e "Please specify the splits, fasta file and database path for processing\n\n"
-	exit 1
+if [ $# -lt 3 ] ; then
+echo "BLASTstampede.sh <number of splits> <fasta file> <full path for blast database>"
+exit 0
 fi
-while :
-do
-    case $1 in
-        -h | --help | -\?)
-            printUsage
-            exit 0
-            ;;
-        -s | --splits)
-            splits=$1
-            shift 1
-            ;;
-        --)
-            shift
-            break
-            ;;
-        -*)
-            printf >&2 'WARNING: Unknown option (ignored): %s\n' "$1"
-            shift
-            ;;
-        *)
-            break
-            ;;
-    esac
-done
-infile="$1"
-blastDB="$2"
 
-inbase=$(basename "${infile%.*}")
+splits="$1"
+infile="$2"
+blastDB="$3"
+
+inbase=$(echo "${infile%.*}")
 cat <<JOBHEAD > parallel_blast.sub
 #!/bin/bash
 #SBATCH -J blast_${inbase}
