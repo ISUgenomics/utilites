021bc4cf0768ce716ba296f850639ef0722c22d0
Arun Seetharam <arnstrm@iastate.edu>
2014-08-20 10:04:07 -0500

    jobs are now divided based on block size

--- BLAST_stampede.sh
+++ BLAST_stampede.sh
@@ -6,6 +6,7 @@
 blastparameters="blastp -max_target_seqs 1 -outfmt 6"
 #
 # NOTE: EXCLUDE '-db' AND '-query' OPTIONS
+# You may also need to change the user email address and account number below
 #
 # Arun Seetharam <arnstrm@iastate.edu>, Genome Informatics Faciltiy
 # 19 August, 2014
@@ -19,13 +20,17 @@ splits="$1"
 infile="$2"
 blastDB="$3"
 
+fsize=$(du -k "$infile" | cut -f 1)
+bsize=$(echo "scale=0; (${fsize} / ${splits})" | bc)
 inbase=$(echo "${infile%.*}")
+
 cat <<JOBHEAD > parallel_blast.sub
 #!/bin/bash
 #SBATCH -J blast_${inbase}
 #SBATCH -o blast_${inbase}.o%j
 #SBATCH -e blast_${inbase}.e%j
 #SBATCH -n ${splits}
+#SBATCH -c 1
 #SBATCH -p normal
 #SBATCH -t 48:00:00
 #SBATCH --mail-user=arnstrm@gmail.com
@@ -36,10 +41,10 @@ module load blast
 module use /home1/02929/arnstrm/programs/modules
 module load parallel
 cat ${infile} | \
-  parallel --jobs ${splits} --block 100k --recstart '>' --pipe \
+  parallel --block ${bsize}k --recstart '>' --pipe \
   ${blastparameters} \
   -db ${blastDB} -query - \
-  > ${inbase}_results.txt
+  >> ${inbase}_results.txt
 JOBHEAD
 
 sbatch parallel_blast.sub
